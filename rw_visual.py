f4ed5855d5fa2396c19613b8f75d7e19e7cd5ab9
Lucyna Mazur <lucyna@studiokreator.com>
2018-09-18 12:49:29 +0200

    style random walk chart

--- rw_visual.py
+++ rw_visual.py
@@ -6,11 +6,26 @@ from random_walk import RandomWalk
 # Keep making new walks, as long as the program is active
 while True:
 
-    # Make a random walk, and plot the points.
-    rw = RandomWalk()
+    # make a random walk, and plot the points.
+    rw = RandomWalk(50000)
     rw.fill_walk()
 
-    plt.scatter(rw.x_values, rw.y_values, s=15)
+    # Set the size of the plotting window
+    plt.figure(dpi=96, figsize=(10, 6))
+
+
+    # render random walk chart and style points
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
+
+    # emphasize the first and last points.
+    plt.scatter(0, 0, c='green', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+
+    # hide axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt._show()
 
     keep_running = input("Utworzyć kolejny wykres? (t/n)")
