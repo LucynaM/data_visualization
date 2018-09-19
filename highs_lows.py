b84e679dcff7935a1e9bc889fa5adc78e96b8b84
Lucyna Mazur <lucyna@studiokreator.com>
2018-09-19 14:06:33 +0200

    style chart

--- highs_lows.py
+++ highs_lows.py
@@ -4,28 +4,33 @@ from datetime import datetime
 
 
 # Get dates, high, and low temperatures from file
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     dates, highs, lows = [], [], []
     for row in reader:
-        date = datetime.strptime(row[0], '%Y-%m-%d')
-        dates.append(date)
-        high = int(row[1])
-        highs.append(high)
-        low = int(row[3])
-        lows.append(low)
+        try:
+            date = datetime.strptime(row[0], '%Y-%m-%d')
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(date, 'missing data')
+        else:
+            dates.append(date)
+            highs.append(high)
+            lows.append(low)
 
 
 # Plot data
-fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(dates, highs, c='red')
-plt.plot(dates, lows, c='blue')
+fig = plt.figure(dpi=96, figsize=(10,6))
+plt.plot(dates, highs, c='red', alpha=0.5)
+plt.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Style plot
-plt.title('Najwyższa i najniższa temperatura dnia, 2014', fontsize=24)
+plt.title('Najwyższa i najniższa temperatura dnia, 2014\nDolina Śmierci, Kalifornia', fontsize=24)
 plt.xlabel("", fontsize=14)
 fig.autofmt_xdate()
 plt.ylabel("Temperatura (F)", fontsize=14)
