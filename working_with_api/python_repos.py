59b63167d67e353c315509b0f52f241983021513
Lucyna Mazur <lucyna@studiokreator.com>
2018-09-22 20:44:38 +0200

    visualization of best rated repos

--- working_with_api/python_repos.py
+++ working_with_api/python_repos.py
@@ -1,22 +1,42 @@
 import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Make an API call, and store the response
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 r = requests.get(url)
-print("Kod stanu: ", r.status_code)
 # Store API response in a variable
 response_dict = r.json()
-print("Całkowita liczba repozytoriów: ", response_dict['total_count'])
 
 # Process results
 repo_dicts = response_dict['items']
-print('Liczba zwróconych repozytoriów: ', len(repo_dicts))
-# Analyze first repositorium
-repo_dict = repo_dicts[0]
-# Show selected information about most rated repos
+
+
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
-    print(repo_dict['name'])
-    print(repo_dict['owner']['login'])
-    print(repo_dict['stargazers_count'])
-    print(repo_dict['html_url'])
-    print(repo_dict['description'])
+    names.append(repo_dict['name'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
+
+# Create chart
+my_style = LS('#333366', base_style=LCS)
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+chart.force_uri_protocol = 'http'
+chart.title = 'Oznaczone największą liczbą gwiazdek projekty Pythona w serwisie Github'
+chart.x_labels = names
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
